a064b87d270d64a156100c0af755f3906f6e1078
Jonas Ruef <jonas.ruef@gmail.com>
2011-07-03 15:18:53 +0200

    Some cleaning, Syso tree works now, tree looks good.

--- pcg/app/algorithms/BooleanTest.java
+++ pcg/app/algorithms/BooleanTest.java
@@ -2,34 +2,28 @@ package algorithms;
 
 /** Copyright 2011 (C) Felix Langenegger & Jonas Ruef */
 
+import helper.BaumgartnerSample;
 import helper.SufTreeNode;
 
 import java.util.ArrayList;
 
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
 
 public class BooleanTest {
 
     private ArrayList<ArrayList<String>> table = new ArrayList<ArrayList<String>>();
-    private ArrayList<ArrayList<String>> generatedFullCoincidenceTable = new ArrayList<ArrayList<String>>();
     private ArrayList<ArrayList<String>> sampleTable = new ArrayList<ArrayList<String>>();
     private ArrayList<ArrayList<String>> msuf = new ArrayList<ArrayList<String>>();
     private ArrayList<ArrayList<String>> sufTable;
     private DefaultTreeModel tree;
-    // How many factors are in given coincidence table
-    private int numberOfFactors = 0;
-    // How many rows must be generated in generateCoincidenceTable()
-    private int coincidenceTableSize = 0;
 
     public BooleanTest(String[][] table) {
 	this.table = ArrayToArrayList(table);
-	this.coincidenceTableSize = (int) Math.pow(2,
-		(this.table.get(0).size() - 1));
-	this.numberOfFactors = (this.table.get(0).size() - 1);
 
-	// generateCoincidenceTable();
+	BaumgartnerSample sample = new BaumgartnerSample();
+	sampleTable = sample.getSampleTable();
 
-	sampleTable();
 	identifySUF();
 	identifyMSUF();
     }
@@ -48,40 +42,37 @@ public class BooleanTest {
     /** Step 3 **/
     public void identifyMSUF() {
 	System.out.println("SufTable:\n" + tableToString(sufTable));
-	ArrayList<ArrayList<String>> tempSufTable = (ArrayList<ArrayList<String>>) sufTable
-		.clone();
 
 	// for (int row = 1; row < sufTable.size(); row++) {
 	ArrayList<String> sufLine = sufTable.get(0);
 
-	// Üse schön rekursiv algorithmus...
+	ArrayList<String> test = new ArrayList<String>();
+	test.add("1");
+	test.add("1");
+	test.add("1");
 
-	ArrayList<String> t = new ArrayList<String>();
-	t.add("1");
-	t.add("1");
-	t.add("1");
-
-	SufTreeNode node = new SufTreeNode(t);
-	tree = new DefaultTreeModel(node);
-	fillUpTree(node);
+	SufTreeNode root = new SufTreeNode(test);
+	tree = new DefaultTreeModel(root);
+	fillUpTree(root);
+	System.out.println("Traversed Tree*********************");
+	traverse(tree);
+	System.out.println("Traversed Tree*********************");
 	// }
 
     }
 
-    private void fillUpTree(SufTreeNode node) {
-	if (node.hasOneCare()) {
-	    // döner
+    private void fillUpTree(SufTreeNode parent) {
+	if (parent.hasOneCare()) {
 	} else {
-	    ArrayList<String> data = node.getData();
-	    ArrayList<Integer> places = node.getCarePlace();
+	    ArrayList<String> data = parent.getData();
+	    ArrayList<Integer> places = parent.getCarePlace();
 	    SufTreeNode newNode;
 	    for (int i = 0; i < places.size(); i++) {
-		ArrayList<String> tmp = permutation(
+		ArrayList<String> curNode = permutation(
 			(ArrayList<String>) data.clone(), places.get(i));
-		newNode = new SufTreeNode(tmp);
-		// newNode.setParent(node);
-		tree.insertNodeInto(newNode, node, i);
-		System.out.println(tmp);
+		newNode = new SufTreeNode(curNode);
+		tree.insertNodeInto(newNode, parent, i);
+		// System.out.println(tmp);
 		fillUpTree(newNode);
 	    }
 	}
@@ -90,44 +81,6 @@ public class BooleanTest {
     private ArrayList<String> permutation(ArrayList<String> data, int place) {
 	data.set(place, "$");
 	return data;
-    }
-
-    /** Generates a full coincidence table with binary-counting method */
-    private void generateCoincidenceTable() {
-	// Generate Binary Numbers
-	generatedFullCoincidenceTable.clear();
-	for (Integer i = 0; i < coincidenceTableSize; i++) {
-	    ArrayList<String> number = new ArrayList<String>();
-	    String binaryNumber = Integer.toBinaryString(i);
-	    int binaryNumberLength = binaryNumber.length();
-
-	    // Add Pre-Zeros, so all binary numbers have the same length
-	    if (binaryNumberLength != (numberOfFactors)) {
-		while (binaryNumber.length() < numberOfFactors) {
-		    binaryNumber = "0" + binaryNumber;
-		}
-	    }
-	    // Set effect for every row to 1
-	    // binaryNumber = binaryNumber + "1";
-
-	    // Add to 2D ArrayList
-	    for (int k = 0; k < binaryNumber.length(); k++) {
-		char oneBit = binaryNumber.charAt(k);
-		number.add(Character.toString(oneBit));
-	    }
-	    generatedFullCoincidenceTable.add(number);
-	}
-    }
-
-    private ArrayList<ArrayList<String>> clone2DArrayList(
-	    ArrayList<ArrayList<String>> toClone) {
-	ArrayList<ArrayList<String>> copy = new ArrayList<ArrayList<String>>();
-	ArrayList<String> col;
-	for (int i = 0; i < toClone.size(); i++) {
-	    col = (ArrayList<String>) toClone.get(i).clone();
-	    copy.add(col);
-	}
-	return copy;
     }
 
     public ArrayList<ArrayList<String>> ArrayToArrayList(String[][] table) {
@@ -152,85 +105,26 @@ public class BooleanTest {
 	return print;
     }
 
-    public void sampleTable() {
-	coincidenceTableSize = (int) Math.pow(2, 4);
-	numberOfFactors = 4;
+    public void traverse(DefaultTreeModel model) {
+	if (model != null) {
+	    Object root = model.getRoot();
+	    System.out.println("Root: " + root.toString());
+	    walk(model, root);
+	} else
+	    System.out.println("Tree is empty.");
+    }
 
-	ArrayList<String> firstRow = new ArrayList<String>();
-	firstRow.add("A");
-	firstRow.add("B");
-	firstRow.add("D");
-	firstRow.add("E");
-	firstRow.add("C");
-	sampleTable.add(firstRow);
-
-	ArrayList<String> secondRow = new ArrayList<String>();
-	secondRow.add("1");
-	secondRow.add("1");
-	secondRow.add("1");
-	secondRow.add("1");
-	secondRow.add("1");
-	sampleTable.add(secondRow);
-
-	ArrayList<String> thirdRow = new ArrayList<String>();
-	thirdRow.add("1");
-	thirdRow.add("1");
-	thirdRow.add("0");
-	thirdRow.add("1");
-	thirdRow.add("1");
-	sampleTable.add(thirdRow);
-
-	ArrayList<String> fourthRow = new ArrayList<String>();
-	fourthRow.add("1");
-	fourthRow.add("0");
-	fourthRow.add("1");
-	fourthRow.add("1");
-	fourthRow.add("1");
-	sampleTable.add(fourthRow);
-
-	ArrayList<String> fifthRow = new ArrayList<String>();
-	fifthRow.add("0");
-	fifthRow.add("1");
-	fifthRow.add("1");
-	fifthRow.add("1");
-	fifthRow.add("1");
-	sampleTable.add(fifthRow);
-
-	ArrayList<String> sixthRow = new ArrayList<String>();
-	sixthRow.add("0");
-	sixthRow.add("1");
-	sixthRow.add("0");
-	sixthRow.add("1");
-	sixthRow.add("1");
-	sampleTable.add(sixthRow);
-
-	ArrayList<String> seventhRow = new ArrayList<String>();
-	seventhRow.add("1");
-	seventhRow.add("0");
-	seventhRow.add("0");
-	seventhRow.add("0");
-	seventhRow.add("1");
-	sampleTable.add(seventhRow);
-
-	ArrayList<String> eigthRow = new ArrayList<String>();
-	eigthRow.add("0");
-	eigthRow.add("0");
-	eigthRow.add("1");
-	eigthRow.add("1");
-	eigthRow.add("0");
-	sampleTable.add(eigthRow);
-
-	ArrayList<String> ninethRow = new ArrayList<String>();
-	ninethRow.add("0");
-	ninethRow.add("0");
-	ninethRow.add("0");
-	ninethRow.add("0");
-	ninethRow.add("0");
-	sampleTable.add(ninethRow);
-
-	/*
-	 * for(int i = 0; i< sampleTable.size(); i++){
-	 * System.out.println("SampleTableRow\n " + sampleTable.get(i)); }
-	 */
+    protected void walk(TreeModel model, Object o) {
+	int cc;
+	cc = model.getChildCount(o);
+	for (int i = 0; i < cc; i++) {
+	    Object child = model.getChild(o, i);
+	    if (model.isLeaf(child))
+		System.out.println(child.toString());
+	    else {
+		System.out.print("------------\n" + child.toString() + " --\n");
+		walk(model, child);
+	    }
+	}
     }
 }
