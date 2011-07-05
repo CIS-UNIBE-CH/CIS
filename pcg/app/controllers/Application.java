398216c614b48326ee6d14c3150f267758d41eac
Jonas Ruef <jonas.ruef@gmail.com>
2011-07-05 21:52:42 +0200

    Plotting of BooleanTest Works

--- pcg/app/controllers/Application.java
+++ pcg/app/controllers/Application.java
@@ -30,61 +30,24 @@ public class Application extends Controller {
     public static void index() {
 	render();
     }
-    
+
     public static void calcBooleanGraph() {
-    	BooleanTest booleanTest = new BooleanTest(generator.getTableAsArray());
-    	
+	timer = new Timer();
+	renderer = new Renderer();
+	BooleanTest booleanTest = new BooleanTest(generator.getTableAsArray());
+	tree = booleanTest.getCnaTree();
+	Long time = timer.timeElapsed();
+	renderer.config(new TreeToGraph(tree));
+	calculatedGraph = tree.toString();
+	calculatedGraphPath = renderer.getImageSource();
+	String elapsedTime = time.toString() + " ms";
+	complexTesting(2, false, null, generatedGraphPath, generatedGraph,
+		calculatedGraphPath, calculatedGraph, "Boolean Test",
+		elapsedTime);
     }
 
     public static void quadroTest(int step, String f1, String f2) {
 	render(step, f1, f2);
-    }
-
-    public static void complexTesting(int step, boolean showTable,
-	    ArrayList<String> table, String generatedGraphPath,
-	    String generatedGraph, String calculatedGraphPath,
-	    String calculatedGraph, String algorithmName, String time) {
-    	
-	render(step, showTable, table, generatedGraphPath, generatedGraph,
-		calculatedGraphPath, calculatedGraph, time, algorithmName);
-    }
-    
-    public static void generateGraph(String numberOfFactors,
-	    String numberOfBundles, String sizeOfBundles) {
-
-	// Generate a Graph with n bundles and a total of k factors
-	int numBundles = Integer.parseInt(numberOfBundles);
-	int numFactors = Integer.parseInt(numberOfFactors);
-	int sizeBundles = Integer.parseInt(sizeOfBundles);
-	renderer = new Renderer();
-	table = new ArrayList<String>();
-
-	if (numFactors >= (2 * numBundles) && numFactors <= 12) {
-	    timer = new Timer();
-	    generator = new RandomGraphGenerator(numBundles, numFactors, sizeBundles);
-	    generatedGraph = generator.getTree().toString();
-	    Long time = timer.timeElapsed();
-	    table = generator.getTable();
-	    renderer.config(new TreeToGraph(generator.getTree()));
-	    generatedGraphPath = renderer.getImageSource();
-
-	    String elapsedTime = time.toString() + " ms";
-
-	    if (numFactors <= 5) {
-		complexTesting(1, true, table, generatedGraphPath,
-				generatedGraph, " ", " ", " ", elapsedTime);
-
-	    } else {
-		complexTesting(1, false, null, generatedGraphPath,
-			generatedGraph, "", "", "", elapsedTime);
-	    }
-
-	} else {
-	    flash.error("Sorry it was not posible to generate a graph, the numbers of factros must be grater than twice as much of the number of bundls.");
-	    params.flash();
-	    complexTesting(0, false, null, generatedGraphPath, generatedGraph,
-		    "", "", "", "");
-	}
     }
 
     public static void calcBinGraph() {
@@ -98,8 +61,56 @@ public class Application extends Controller {
 	calculatedGraphPath = renderer.getImageSource();
 	String elapsedTime = time.toString() + " ms";
 	complexTesting(2, false, null, generatedGraphPath, generatedGraph,
-		calculatedGraphPath, calculatedGraph, "Binary Testing",
+		calculatedGraphPath, calculatedGraph, "Binary Test",
 		elapsedTime);
+    }
+
+    public static void complexTesting(int step, boolean showTable,
+	    ArrayList<String> table, String generatedGraphPath,
+	    String generatedGraph, String calculatedGraphPath,
+	    String calculatedGraph, String algorithmName, String time) {
+
+	render(step, showTable, table, generatedGraphPath, generatedGraph,
+		calculatedGraphPath, calculatedGraph, time, algorithmName);
+    }
+
+    public static void generateGraph(String numberOfFactors,
+	    String numberOfBundles, String sizeOfBundles) {
+
+	// Generate a Graph with n bundles and a total of k factors
+	int numBundles = Integer.parseInt(numberOfBundles);
+	int numFactors = Integer.parseInt(numberOfFactors);
+	int sizeBundles = Integer.parseInt(sizeOfBundles);
+	renderer = new Renderer();
+	table = new ArrayList<String>();
+
+	if (numFactors >= (2 * numBundles) && numFactors <= 12) {
+	    timer = new Timer();
+	    generator = new RandomGraphGenerator(numBundles, numFactors,
+		    sizeBundles);
+	    generatedGraph = generator.getTree().toString();
+	    Long time = timer.timeElapsed();
+	    table = generator.getTable();
+	    renderer.config(new TreeToGraph(generator.getTree()));
+	    generatedGraphPath = renderer.getImageSource();
+
+	    String elapsedTime = time.toString() + " ms";
+
+	    if (numFactors <= 5) {
+		complexTesting(1, true, table, generatedGraphPath,
+			generatedGraph, " ", " ", " ", elapsedTime);
+
+	    } else {
+		complexTesting(1, false, null, generatedGraphPath,
+			generatedGraph, "", "", "", elapsedTime);
+	    }
+
+	} else {
+	    flash.error("Sorry it was not posible to generate a graph, the numbers of factros must be grater than twice as much of the number of bundls.");
+	    params.flash();
+	    complexTesting(0, false, null, generatedGraphPath, generatedGraph,
+		    "", "", "", "");
+	}
     }
 
     public static void calccalcEQuadroGraph() {
