053ea1da593496ca192e8e4b5a0e602c375ccec8
Felix Langenegger <felix.langenegger@gmail.com>
2011-07-24 20:04:13 +0200

    all stuff handled?

--- pcg/app/controllers/CNAController.java
+++ pcg/app/controllers/CNAController.java
@@ -62,7 +62,9 @@ public class CNAController extends Controller {
 	    String generatedGraphSource = renderer.getImageSource();
 	    String generatedGraphString = theories.toString();
 
-	    render(generatedGraphSource, generatedGraphString);
+	    boolean calc = (theories.getAllNames().size() < 7);
+	    System.out.println(calc);
+	    render(calc, generatedGraphSource, generatedGraphString);
 
 	} catch (OutOfMemoryError e) {
 	    flash.error("Phuu! This calculation was to complex! "
@@ -71,10 +73,13 @@ public class CNAController extends Controller {
 	    params.flash();
 	    setup();
 	    e.printStackTrace();
-	}
-
-	catch (IllegalArgumentException e) {
+	} catch (IllegalArgumentException e) {
 	    flash.error("You calculated a null graph ;)");
+	    params.flash();
+	    setup();
+	    e.printStackTrace();
+	} catch (IndexOutOfBoundsException e) {
+	    flash.error("Too much data!");
 	    params.flash();
 	    setup();
 	    e.printStackTrace();
@@ -153,6 +158,11 @@ public class CNAController extends Controller {
     public static void inputTable(String table) {
 
 	CNATable cnatable = new CNATable("\r\n", ",", table);
+	if (cnatable.get(0).size() > 7) {
+	    flash.error("Sorry! Not more than 7 factors are allowed.");
+	    params.flash();
+	    setup();
+	}
 	try {
 	    timer = new Timer();
 	    CNAlgorithm cnaAlgorithm = new CNAlgorithm(cnatable);
@@ -191,33 +201,48 @@ public class CNAController extends Controller {
 	    flash.error("Sorry, something went very wrong!");
 	    params.flash();
 	    setup();
-
 	}
     }
 
     public static void inputMT(String mtset) {
-	CNAList list = new CNAList("\r\n", mtset);
-	CNAList factors;
-	theories = new MinimalTheorySet();
-	MinimalTheory theorie;
-	for (String str : list) {
-	    factors = new CNAList();
-	    String[] array = str.split("=>");
-	    String[] fac = array[0].split("v");
-	    for (int i = 0; i < fac.length; i++) {
-		factors.add(fac[i]);
+	try {
+	    CNAList list = new CNAList("\r\n", mtset);
+	    CNAList factors;
+	    theories = new MinimalTheorySet();
+	    MinimalTheory theorie;
+	    for (String str : list) {
+		factors = new CNAList();
+		String[] array = str.split("=>");
+		String[] fac = array[0].split("v");
+		for (int i = 0; i < fac.length; i++) {
+		    factors.add(fac[i]);
+		}
+		theorie = new MinimalTheory(factors, array[1]);
+		theories.add(theorie);
 	    }
-	    theorie = new MinimalTheory(factors, array[1]);
-	    theories.add(theorie);
+	    Graph graph = new Graph(theories);
+	    Renderer renderer = new Renderer();
+	    renderer.setShowEdgeLabels(showBundleNumRenderer);
+	    renderer.config(graph);
+
+	    String generatedGraphSource = renderer.getImageSource();
+	    String generatedGraphString = theories.toString();
+	    boolean calc = (theories.getAllNames().size() < 7);
+
+	    render(generatedGraphSource, generatedGraphString, calc);
+	} catch (ArrayIndexOutOfBoundsException e) {
+	    flash.error("Wrong MT Syntax!");
+	    params.flash();
+	    setup();
+	} catch (IndexOutOfBoundsException e) {
+	    flash.error("Please give us more data!");
+	    params.flash();
+	    setup();
+	} catch (IllegalArgumentException e) {
+	    flash.error("Sorry, something went very wrong!");
+	    params.flash();
+	    setup();
+
 	}
-	Graph graph = new Graph(theories);
-	Renderer renderer = new Renderer();
-	renderer.setShowEdgeLabels(showBundleNumRenderer);
-	renderer.config(graph);
-
-	String generatedGraphSource = renderer.getImageSource();
-	String generatedGraphString = theories.toString();
-
-	render(generatedGraphSource, generatedGraphString);
     }
 }
