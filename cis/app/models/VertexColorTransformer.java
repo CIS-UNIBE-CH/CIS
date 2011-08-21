fea34e3d1114b94db063652c49db4809428962ba
Jonas Ruef <jonas.ruef@gmail.com>
2011-08-21 10:07:59 +0200

    First modifications on node coloring

--- cis/app/models/VertexColorTransformer.java
+++ cis/app/models/VertexColorTransformer.java
@@ -13,29 +13,26 @@ import datastructures.graph.Node;
  * bundle is cause or effect.
  */
 public class VertexColorTransformer implements Transformer<Node, Paint> {
-    private ArrayList<Color> colors = new ArrayList<Color>();
+    private final ArrayList<Color> colors = new ArrayList<Color>();
 
     @Override
     public Paint transform(Node node) {
 	colors.clear();
 	colors.add(Color.lightGray);
 	colors.add(Color.magenta);
-	colors.add(Color.pink);
-	colors.add(Color.red);
+	colors.add(Color.green);
+	colors.add(Color.gray);
 	colors.add(Color.orange);
+	colors.add(Color.red);
 	colors.add(Color.cyan);
+	colors.add(Color.pink);
 	colors.add(Color.yellow);
 
 	if (node.isEffect()) {
 	    return new Color(255, 153, 0);
 	} else {
-	    if (node.isPartOfBundle()) {
-		int bundleNumber = node.getBundle();
-		return colors.get((bundleNumber - 1) % 8);
-
-	    } else {
-		return Color.GREEN;
-	    }
+	    int bundleNumber = node.getBundle();
+	    return colors.get((bundleNumber % 9) - 1);
 	}
     }
 }
